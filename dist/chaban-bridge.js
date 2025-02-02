fad2688d6f9b6e5851d1b2ea0cef63b9c01262f0
LightD31 <totodetoulouse@hotmail.fr>
2025-02-02 20:35:34 +0100

    Mise à jour des options de mise en page dans ChabanBridgeCard pour augmenter la flexibilité de la grille

--- dist/chaban-bridge.js
+++ dist/chaban-bridge.js
@@ -251,10 +251,10 @@ class ChabanBridgeCard extends LitElement {
 
   getLayoutOptions() {
     return {
-      grid_rows: 4,
-      grid_columns: 2,
-      grid_min_rows: 3,
-      grid_max_rows: 6,
+      grid_rows: 6,
+      grid_columns: 4,
+      grid_min_rows: 6,
+      grid_max_rows: 10,
     };
   }
 }
